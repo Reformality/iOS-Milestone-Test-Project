3c072dd4afa0e252f11d5bdaa525112c33953776
Will Wu <Will@Wills-MacBook-Air.local>
2021-07-18 16:16:20 -0700

    Created lesson 2 project

---
+++ Chapter 2 APP/Chapter 2 APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chapter 2 APP
+//
+//  Created by Will Wu on 7/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
